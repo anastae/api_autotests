7ca4f4294b6d77abadf97b9d3810a9397db48d33
anastas <anastasys.269@gmail.com>
2026-01-09 12:26:25 +0300

    added markers for auth tests

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,4 +1,6 @@
 from http import HTTPStatus
+import pytest
+
 from clients.users.public_users_client import get_public_users_client
 from clients.authentication.authentication_client import get_authentication_client
 from clients.users.users_schema import CreateUserRequestSchema
@@ -7,14 +9,16 @@ from tools.assertions.base import assert_status_code
 from tools.assertions.authentification import assert_login_response
 from tools.assertions.schema import validate_json_schema
 
+@pytest.mark.authentication
+@pytest.mark.regression
 def test_login():
     public_users_client = get_public_users_client()
     authentification_client = get_authentication_client()
 
     request_user = CreateUserRequestSchema()
-    user = public_users_client.create_user(request_user)
+    public_users_client.create_user(request_user)
 
-    request = LoginRequestSchema(email=user.email, password=request_user.password)
+    request = LoginRequestSchema(email=request_user.email, password=request_user.password)
     response = authentification_client.login_api(request)
     response_data = LoginResponseSchema.model_validate_json(response.text)
 
