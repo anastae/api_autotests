94a2258df49d689b24fe212c5a4648e84c19083c
anastas <anastasys.269@gmail.com>
2026-01-11 11:33:07 +0300

    parametrized test_create_user for different domains in email

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,18 +1,22 @@
 from http import HTTPStatus
 import pytest
 
+from clients.users.public_users_client import PublicUsersClient
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema
 from tools.assertions.schema import validate_json_schema
 from tools.assertions.base import assert_status_code
 from tools.assertions.users import assert_create_user_response, assert_get_user_response
 # from fixtures.users import UserFixture
+from tools.fakers import fake
 
 @pytest.mark.users
 @pytest.mark.regression
-def test_create_user(public_users_client):
-
+@pytest.mark.parametrize('email', ['mail.ru', 'gmail.com', 'example.com'])
+def test_create_user(email: str, public_users_client: PublicUsersClient):
+    # генерируем почту с доменами из параметризации
+    email = fake.email(domain=email)
     # Формируем тело запроса на создание пользователя
-    request = CreateUserRequestSchema()
+    request = CreateUserRequestSchema(email=email)
     # Отправляем запрос на создание пользователя
     response = public_users_client.create_user_api(request)
     # Инициализируем модель ответа на основе полученного JSON в ответе
