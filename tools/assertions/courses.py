3ed54ec0a1ba0628f0c2c5d3195769195fe8fbf7
anastas <anastasys.269@gmail.com>
2026-01-13 15:31:15 +0300

    added test_create_course

--- tools/assertions/courses.py
+++ tools/assertions/courses.py
@@ -1,5 +1,6 @@
 from clients.courses.courses_schema import (UpdateCourseRequestSchema, UpdateCourseResponseSchema, CourseSchema,
-                                            GetCoursesResponseSchema, CreateCourseResponseSchema)
+                                            GetCoursesResponseSchema, CreateCourseResponseSchema,
+                                            CreateCourseRequestSchema)
 from tools.assertions.base import assert_equal, assert_length
 from tools.assertions.files import assert_file
 from tools.assertions.users import assert_user
@@ -23,6 +24,21 @@ def assert_course(actual: CourseSchema, expected: CourseSchema):
     # проверяем вложенные объекты
     assert_file(actual.preview_file, expected.preview_file)
     assert_user(actual.created_by_user, expected.created_by_user)
+
+def assert_create_course_response(request: CreateCourseRequestSchema, response: CreateCourseResponseSchema):
+    """
+    Проверяет, что ответ на создание курса соответствует запросу
+    :param request: Исходный запрос на создание файла.
+    :param response: Ответ API с данными файла.
+    :raises AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    assert_equal(response.course.title, request.title, "title")
+    assert_equal(response.course.max_score, request.max_score, "max_score")
+    assert_equal(response.course.min_score, request.min_score, "min_score")
+    assert_equal(response.course.description, request.description, "description")
+    assert_equal(response.course.estimated_time, request.estimated_time, "estimated_time")
+    assert_equal(response.course.preview_file.id, request.preview_file_id, "preview_file_id")
+    assert_equal(response.course.created_by_user.id, request.created_by_user_id, "created_by_user_id")
 
 def assert_get_courses_response(
         get_courses_response: GetCoursesResponseSchema,
