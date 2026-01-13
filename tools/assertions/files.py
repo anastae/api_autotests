0ad4d0ce7302a4b242222f48409c93140aef29ab
anastas <anastasys.269@gmail.com>
2026-01-13 12:28:31 +0300

    added errors schema, errors assertions, negative file tests

--- tools/assertions/files.py
+++ tools/assertions/files.py
@@ -1,6 +1,9 @@
 import httpx
-from clients.files.files_schema import CreateFileResponseSchema, CreateFileRequestSchema
+
+from clients.errors_schema import ValidationErrorResponseSchema, ValidationErrorSchema
+from clients.files.files_schema import CreateFileResponseSchema, CreateFileRequestSchema, FileSchema, GetFileResponseSchema
 from tools.assertions.base import assert_equal
+from tools.assertions.errors import assert_validation_error_response
 
 
 def assert_create_file_response(request: CreateFileRequestSchema, response: CreateFileResponseSchema):
@@ -26,5 +29,73 @@ def assert_file_is_accessible(url: str):
     :param url: Ссылка на файл.
     :raises AssertionError: Если файл не доступен.
     """
-    response = httpx.get(url)
+    response = httpx.get(str(url))
     assert response.status_code == 200, f"Файл недоступен по URL: {url}"
+
+def assert_file(actual: FileSchema, expected: FileSchema):
+    """
+        Проверяет, что фактические данные файла соответствуют ожидаемым.
+
+        :param actual: Фактические данные файла.
+        :param expected: Ожидаемые данные файла.
+        :raises AssertionError: Если хотя бы одно поле не совпадает.
+        """
+    assert_equal(actual.id, expected.id, "id")
+    assert_equal(actual.url, expected.url, "url")
+    assert_equal(actual.filename, expected.filename, 'filename')
+    assert_equal(actual.directory, expected.directory, f'directory')
+
+def assert_get_file_response(
+        get_file_response: GetFileResponseSchema,
+        create_file_response: CreateFileResponseSchema
+):
+    """
+        Проверяет, что ответ на получение файла соответствует ответу на его создание.
+
+        :param get_file_response: Ответ API при запросе данных файла.
+        :param create_file_response: Ответ API при создании файла.
+        :raises AssertionError: Если данные файла не совпадают.
+        """
+    assert_equal(get_file_response.file, create_file_response.file, "file")
+
+def assert_create_file_with_empty_filename_response(actual: ValidationErrorResponseSchema):
+    """
+    Проверяет, что ответ на создание файла с пустым именем файла соответствует ожидаемой валидационной ошибке.
+
+    :param actual: Ответ от API с ошибкой валидации, который необходимо проверить.
+    :raises AssertionError: Если фактический ответ не соответствует ожидаемому.
+    """
+    expected = ValidationErrorResponseSchema(
+        details=[
+            ValidationErrorSchema(
+                type="string_too_short",  # Тип ошибки, связанной с слишком короткой строкой.
+                input="",  # Пустое имя файла.
+                context={"min_length": 1},  # Минимальная длина строки должна быть 1 символ.
+                message="String should have at least 1 character",  # Сообщение об ошибке.
+                location=["body", "filename"]  # Ошибка возникает в теле запроса, поле "filename".
+            )
+        ]
+    )
+    assert_validation_error_response(actual, expected)
+
+
+def assert_create_file_with_empty_directory_response(actual: ValidationErrorResponseSchema):
+    """
+    Проверяет, что ответ на создание файла с пустым значением директории соответствует ожидаемой валидационной ошибке.
+
+    :param actual: Ответ от API с ошибкой валидации, который необходимо проверить.
+    :raises AssertionError: Если фактический ответ не соответствует ожидаемому.
+    """
+    expected = ValidationErrorResponseSchema(
+        details=[
+            ValidationErrorSchema(
+                type="string_too_short",  # Тип ошибки, связанной с слишком короткой строкой.
+                input="",  # Пустая директория.
+                context={"min_length": 1},  # Минимальная длина строки должна быть 1 символ.
+                message="String should have at least 1 character",  # Сообщение об ошибке.
+                location=["body", "directory"]  # Ошибка возникает в теле запроса, поле "directory".
+            )
+        ]
+    )
+    assert_validation_error_response(actual, expected)
+
