c0d2cff33648c0cb7bd7102d46f88a8f5f5aa1ba
anastas <anastasys.269@gmail.com>
2026-01-13 15:09:48 +0300

    added test_get_courses

---
+++ tests/courses/test_courses.py
@@ -0,0 +1,48 @@
+from http import HTTPStatus
+import pytest
+
+from clients.courses.courses_client import CoursesClient
+from clients.courses.courses_schema import (UpdateCourseRequestSchema,
+                                            UpdateCourseResponseSchema, GetCoursesQuerySchema,
+                                            GetCoursesResponseSchema)
+from fixtures.courses import CourseFixture
+from fixtures.users import UserFixture
+from tools.assertions.base import assert_status_code
+from tools.assertions.courses import assert_update_course_response, assert_get_courses_response
+from tools.assertions.schema import validate_json_schema
+
+
+@pytest.mark.courses
+@pytest.mark.regression
+class TestCourses:
+    def test_get_courses(self,
+                         courses_client: CoursesClient,
+                         function_user: UserFixture,
+                         function_course: CourseFixture
+                         ):
+        # Формируем параметры запроса, передавая user_id
+        query = GetCoursesQuerySchema(user_id=function_user.response.user.id)
+        response = courses_client.get_courses_api(query)
+        response_data = GetCoursesResponseSchema.model_validate_json(response.text)
+
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        assert_get_courses_response(response_data, [function_course.response])
+        validate_json_schema(response.json(), response_data.model_json_schema())
+
+    def test_update_course(self, courses_client: CoursesClient, function_course: CourseFixture):
+        # Формируем данные для обновления
+        request = UpdateCourseRequestSchema()
+        # Отправляем запрос на обновление курса
+        response = courses_client.update_course_api(function_course.response.course.id, request)
+        # Преобразуем JSON-ответ в объект схемы
+        response_data = UpdateCourseResponseSchema.model_validate_json(response.text)
+
+        # Проверяем статус-код ответа
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        # Проверяем, что данные в ответе соответствуют запросу
+        assert_update_course_response(request, response_data)
+
+        #добавить проверку на гет что действительно обновился курс
+
+        # Валидируем JSON-схему ответа
+        validate_json_schema(response.json(), response_data.model_json_schema())
