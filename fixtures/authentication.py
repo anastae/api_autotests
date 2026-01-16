0c212e8644bb36a6f82cb92b6be04d3238cc11dc
anastae <anastae@users.noreply.github.com>
2026-01-16 06:50:56 +0000

    deploy: b87787ffffdd32f9512aac507c5f3bca9908bf9e

--- fixtures/authentication.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from clients.authentication.authentication_client import AuthenticationClient, get_authentication_client
-
-
-@pytest.fixture
-def authentication_client() -> AuthenticationClient:
-    # Создаем новый API клиент для работы с аутентификацией
-    return get_authentication_client()
